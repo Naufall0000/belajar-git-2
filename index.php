3913acc7be91842e799b8ffd4a4359b714faec85
Naufall0000 <swastikanaufal@gmail.com>
2025-04-07 21:14:48 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello world</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut nisi nam, eius tempora incidunt animi itaque id reiciendis voluptate. Quo quis totam id, quam sequi neque cum! Quam, recusandae. Cumque?</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+    </ul>
 </body>
 </html>
