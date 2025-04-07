4aa000a68910acabc9209f722138786589bcfe5a
Naufall0000 <swastikanaufal@gmail.com>
2025-04-07 21:04:44 +0800

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut nisi nam, eius tempora incidunt animi itaque id reiciendis voluptate. Quo quis totam id, quam sequi neque cum! Quam, recusandae. Cumque?</p>
 </body>
 </html>
